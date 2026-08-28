685a72b540ba97ab3af9a24ce000dfbb0e80d2d7
farshadhelboys-crypto <farshadhelboys@gmail.com>
2026-08-28 23:38:05 +0330

    Update MainScreen.kt

--- composeApp/src/commonMain/kotlin/io/github/immaghzbad/aetherst/ui/screens/MainScreen.kt
+++ composeApp/src/commonMain/kotlin/io/github/immaghzbad/aetherst/ui/screens/MainScreen.kt
@@ -1,7 +1,6 @@
 ﻿package io.github.immaghzbad.aetherst.shared.ui.screens
 
 import io.github.immaghzbad.aetherst.shared.ui.theme.AppPalette
-import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.spring
