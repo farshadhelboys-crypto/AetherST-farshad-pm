3106b48302b196ef1f0957d125996403a263d360
farshadhelboys-crypto <farshadhelboys@gmail.com>
2026-08-29 12:22:43 +0330

    Update Color.kt

--- composeApp/src/commonMain/kotlin/io/github/immaghzbad/aetherst/ui/theme/Color.kt
+++ composeApp/src/commonMain/kotlin/io/github/immaghzbad/aetherst/ui/theme/Color.kt
@@ -5,45 +5,45 @@ import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.graphics.Color
 
-val ElegantPrimary = Color(0xFFD0BCFF)
-val ElegantOnPrimary = Color(0xFF381E72)
-val ElegantPrimaryContainer = Color(0xFF381E72)
-val ElegantOnPrimaryContainer = Color(0xFFEADDFF)
-val ElegantSecondary = Color(0xFFCCC2DC)
+val ElegantPrimary = Color(0xFFD4AF37)
+val ElegantOnPrimary = Color(0xFF000000)
+val ElegantPrimaryContainer = Color(0xFF3A3120)
+val ElegantOnPrimaryContainer = Color(0xFFF0D77B)
+val ElegantSecondary = Color(0xFF9A9A94)
 val ElegantBackground = Color(0xFF000000)
 val ElegantSurface = Color(0xFF000000)
-val ElegantSurfaceCard = Color(0xFF1C1B1F)
-val ElegantSurfaceActive = Color(0xFF4A4458)
-val ElegantOutline = Color(0xFF49454F)
-val ElegantTextPrimary = Color(0xFFE6E1E5)
-val ElegantTextSecondary = Color(0xFFCAC4D0)
+val ElegantSurfaceCard = Color(0xFF141414)
+val ElegantSurfaceActive = Color(0xFF2A2A2A)
+val ElegantOutline = Color(0xFF2A2A2A)
+val ElegantTextPrimary = Color(0xFFF5F5F0)
+val ElegantTextSecondary = Color(0xFF9A9A94)
 val ConnectedGreen = Color(0xFF81C784)
 val ScanningAmber = Color(0xFFFFB74D)
 val ErrorRed = Color(0xFFF2B8B5)
 
 object AppPalette {
-    val accent = Color(0xFF007AFF)
-    val accentVariant = Color(0xFF5856D6)
-    val accentVariantAlt = Color(0xFFAF52DE)
-    val onAccent = Color(0xFFFFFFFF)
+    val accent = Color(0xFFD4AF37)
+    val accentVariant = Color(0xFFB8860B)
+    val accentVariantAlt = Color(0xFFF0D77B)
+    val onAccent = Color(0xFF000000)
 
     val statusConnected = Color(0xFF34C759)
     val statusScanning = Color(0xFFFF9500)
     val statusError = Color(0xFFFF3B30)
     val debugCyan = Color(0xFF64D2FF)
 
-    val surfaceRaised = Color(0xFF1C1C1E)
-    val surfaceSunken = Color(0xFF0A0A0D)
-    val groupBg = Color(0xFF2C2C2E)
-    val divider = Color(0xFF2C2C2E)
-    val inactiveTrack = Color(0xFF3A3A3C)
+    val surfaceRaised = Color(0xFF141414)
+    val surfaceSunken = Color(0xFF000000)
+    val groupBg = Color(0xFF1A1A1A)
+    val divider = Color(0xFF2A2A2A)
+    val inactiveTrack = Color(0xFF333333)
 
-    val textPrimary = Color(0xFFECECF1)
-    val textSecondary = Color(0xFF8E8E93)
+    val textPrimary = Color(0xFFF5F5F0)
+    val textSecondary = Color(0xFF9A9A94)
 
-    val navBackground = Color(0xFF1C1C1E)
-    val navActive = Color(0xFF007AFF)
-    val navInactive = Color(0xFF8E8E93)
+    val navBackground = Color(0xFF141414)
+    val navActive = Color(0xFFD4AF37)
+    val navInactive = Color(0xFF6B6B66)
 }
 
 @Immutable
@@ -88,23 +88,23 @@ internal val darkAppColors = AppColors(
 )
 
 internal val lightAppColors = AppColors(
-    accent = Color(0xFF0A6CFF),
+    accent = Color(0xFFB8860B),
     onAccent = Color(0xFFFFFFFF),
-    accentVariant = Color(0xFF5E5CE6),
+    accentVariant = Color(0xFF8C6A0E),
     debugCyan = Color(0xFF0A84FF),
     surfaceRaised = Color(0xFFFFFFFF),
-    surfaceSunken = Color(0xFFF2F3F7),
-    groupBg = Color(0xFFECECF1),
-    divider = Color(0xFFDCDCE2),
-    inactiveTrack = Color(0xFFD1D1D6),
-    textPrimary = Color(0xFF1A1A1F),
-    textSecondary = Color(0xFF5A5A63),
+    surfaceSunken = Color(0xFFF7F6F2),
+    groupBg = Color(0xFFEFEDE6),
+    divider = Color(0xFFE0DED5),
+    inactiveTrack = Color(0xFFD1CFC6),
+    textPrimary = Color(0xFF1A1A17),
+    textSecondary = Color(0xFF6B6B63),
     statusConnected = Color(0xFF1E9E6B),
     statusScanning = Color(0xFFC77D1E),
     statusError = Color(0xFFC0392B),
-    navBackground = Color(0xFFF2F3F7),
-    navActive = Color(0xFF0A6CFF),
-    navInactive = Color(0xFF8A8A95),
+    navBackground = Color(0xFFF7F6F2),
+    navActive = Color(0xFFB8860B),
+    navInactive = Color(0xFF8A8A80),
 )
 
 val LocalAppColors = staticCompositionLocalOf { darkAppColors }
