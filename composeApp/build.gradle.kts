f90d251b9b7e4c079b8a33a07f8fe1f689730a3e
farshadhelboys-crypto <farshadhelboys@gmail.com>
2026-09-02 00:56:05 +0330

    Update build.gradle.kts

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.compose)
-    // alias(libs.plugins.kotlin.compose)  // ← حذف کن
     alias(libs.plugins.kotlin.serialization)
 }
 
