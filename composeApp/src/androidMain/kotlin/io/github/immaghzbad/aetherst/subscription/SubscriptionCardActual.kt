de714d2525059504a64dc95594f6bc1cd2d6e80a
farshadhelboys-crypto <farshadhelboys@gmail.com>
2026-09-02 11:07:26 +0330

    Delete composeApp/src/androidMain/kotlin/io/github/immaghzbad/aetherst/subscription/SubscriptionCardActual.kt

--- composeApp/src/androidMain/kotlin/io/github/immaghzbad/aetherst/subscription/SubscriptionCardActual.kt
+++
@@ -1,9 +0,0 @@
-package io.github.immaghzbad.aetherst.shared.ui.screens
-
-import androidx.compose.runtime.Composable
-import io.github.immaghzbad.aetherst.subscription.SubscriptionCard
-
-@Composable
-actual fun PlatformSubscriptionCard() {
-    SubscriptionCard()
-}
