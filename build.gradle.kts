2e7ad6d79c949e2570077e6151cd6c5a6e3757e2
farshadhelboys-crypto <farshadhelboys@gmail.com>
2026-09-02 09:39:58 +0330

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,3 @@
-
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
@@ -9,4 +8,5 @@ plugins {
     alias(libs.plugins.google.devtools.ksp) apply false
     alias(libs.plugins.roborazzi) apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
+    alias(libs.plugins.android.kotlin.multiplatform.library) apply false
 }
